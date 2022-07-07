66358167757f2ea2b942a143b9cde1ae8c348439
Tai <tainancmoraes@gmail.com>
2022-07-07 11:09:32 -0300

    alter table

--- src/main.js
+++ src/main.js
@@ -2,5 +2,5 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
-
+import '@/assets/css/style.scss';
 createApp(App).use(router).mount('#app')
